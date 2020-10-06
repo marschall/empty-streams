aa269f3578d9e8eeb79ee02dfb1b81f6534f221b
Philippe Marschall <philippe.marschall@netcetera.com>
2020-10-06 17:45:04 +0200

    doc

---
+++ README.md
@@ -0,0 +1,6 @@
+Empty Streams
+=============
+
+Empty implementations of the Java `java.util.stream.Stream`, `java.util.stream.IntStream`, `java.util.stream.LongStream` and `java.util.stream.DoubleStream`
+
+Compared to `java.util.stream.Stream#empty()`, `java.util.stream.IntStream#empty()`, `java.util.stream.LongStream#empty()` and `java.util.stream.DoubleStream#empty()` these implementations can be more efficient.
--- src/main/java/com/github/marschall/emptystreams/EmptyStreams.java
+++ src/main/java/com/github/marschall/emptystreams/EmptyStreams.java
@@ -1,6 +1,9 @@
 package com.github.marschall.emptystreams;
 
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 /**
  * Class with factory methods for empty streams.
@@ -11,8 +14,45 @@ public final class EmptyStreams {
     throw new AssertionError("not instantiable");
   }
 
+  /**
+   * Returns and empty {@link IntStream}.
+   * 
+   * @return an empty stream
+   * @see IntStream#empty()
+   */
   public static IntStream emptyIntStream() {
     return new EmptyIntStream();
+  }
+
+  /**
+   * Returns and empty {@link LongStream}.
+   * 
+   * @return an empty stream
+   * @see LongStream#empty()
+   */
+  public static LongStream emptyLongStream() {
+    return new EmptyLongStream();
+  }
+
+  /**
+   * Returns and empty {@link DoubleStream}.
+   * 
+   * @return an empty stream
+   * @see DoubleStream#empty()
+   */
+  public static DoubleStream emptyDoubleStream() {
+    return new EmptyDoubleStream();
+  }
+
+  /**
+   * Returns and empty {@link Stream}.
+   * 
+   * @return an empty stream
+   * @param <T> the type of the stream elements
+   * @see Stream#empty()
+   */
+  public static <T> Stream<T> emptyStream() {
+    return new EmptyStream<>();
   }
 
 }
